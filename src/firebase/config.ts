9132ca43815e40a1a9408198cd3eabe7fe15d524
WilhelmDev <wiljoseflores@gmail.com>
2023-11-09 19:33:07 -0400

    feature: add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQH0DnxqzfHDHf4Yx_h88e_hhwhS9Rd9U",
+    authDomain: "restaurant-b942b.firebaseapp.com",
+    projectId: "restaurant-b942b",
+    storageBucket: "restaurant-b942b.appspot.com",
+    messagingSenderId: "1067359546963",
+    appId: "1:1067359546963:web:2da390307f0188d30660a6",
+    measurementId: "G-7Y2Q23V6W7"
+};
+
+export default firebaseConfig
