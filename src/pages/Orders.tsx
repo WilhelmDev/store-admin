43fa11856d98032d9de0965e96d8a7a83ebfe3fe
WilhelmDev <wiljoseflores@gmail.com>
2024-02-02 20:23:46 -0400

    feat: add table basic

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,5 +1,6 @@
-import { DocumentPlusIcon, ShoppingCartIcon } from "@heroicons/react/24/solid";
-import { Button, Card, CardHeader, Tab, Tabs, TabsHeader, Typography } from "@material-tailwind/react"
+import { ChevronUpDownIcon, DocumentMagnifyingGlassIcon, DocumentPlusIcon, MagnifyingGlassIcon, ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { Button, Card, CardBody, CardHeader, Chip, IconButton, Input, Tab, Tabs, TabsHeader, Tooltip, Typography } from "@material-tailwind/react"
+import { billData } from "../variables";
 
 export const Bills = () => {
 
@@ -17,6 +18,8 @@ export const Bills = () => {
       value: "all",
     },
   ];
+
+  const TABLE_HEAD = ["Encargado", "Fecha", "Tipo", "Total", "Opciones"];
 
   return (
     <Card className="h-full w-full px-2 pt-3" shadow={false}>
@@ -46,9 +49,98 @@ export const Bills = () => {
                 ))}
               </TabsHeader>
             </Tabs>
+            <div className="w-full md:w-72">
+              <Input
+                crossOrigin={undefined}
+                label="Search"
+                icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+              />
+            </div>
           </section>
         </main>
       </CardHeader>
+      <CardBody className=" overflow-auto ">
+        <table className="mt-4 w-full table-auto text-left">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head, index) => (
+                <th 
+                key={head} 
+                className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors rounded hover:bg-blue-gray-50">
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                  >
+                    {head}{" "}
+                    {index !== TABLE_HEAD.length - 1 && (
+                      <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                    )}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {billData.map(({ user, type, date }, index) => {
+              const isLast = index === billData.length - 1;
+              const classes = isLast
+                ? "p-4"
+                : "p-4 border-b border-blue-gray-50";
+
+              return (
+                <tr key={index}>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {user}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {date}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <div className="w-max">
+                      <Chip
+                        variant="ghost"
+                        size="sm"
+                        value={type === 'sell' ? "Venta" : "Ingreso"}
+                        color={type === 'sell' ? "green" : "yellow"}
+                      />
+                    </div>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      100$
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Tooltip content="Ver Detalles">
+                      <IconButton variant="text">
+                        <DocumentMagnifyingGlassIcon className="h-4 w-4" />
+                      </IconButton>
+                    </Tooltip>
+                  </td>
+                </tr>
+              );
+            },
+          )}
+        </tbody>
+        </table>
+      </CardBody>
     </Card>
   )
 }
--- src/variables/index.ts
+++ src/variables/index.ts
@@ -28,3 +28,51 @@ export const categories = [
         value: 'otros'
     }
 ]
+
+export const billData = [
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
+        user: "John Michael",
+        email: "john@creative-tim.com",
+        type: "Manager",
+        org: "sell",
+        online: true,
+        date: "23/04/18",
+      },
+      {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
+        user: "Alexa Liras",
+        email: "alexa@creative-tim.com",
+        type: "sell",
+        org: "Developer",
+        online: false,
+        date: "23/04/18",
+      },
+      {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
+        user: "Laurent Perrier",
+        email: "laurent@creative-tim.com",
+        type: "buy",
+        org: "Projects",
+        online: false,
+        date: "19/09/17",
+      },
+      {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
+        user: "Michael Levi",
+        email: "michael@creative-tim.com",
+        type: "sell",
+        org: "Developer",
+        online: true,
+        date: "24/12/08",
+      },
+      {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
+        user: "Richard Gran",
+        email: "richard@creative-tim.com",
+        type: "sell",
+        org: "Executive",
+        online: false,
+        date: "04/10/21",
+      },
+]
